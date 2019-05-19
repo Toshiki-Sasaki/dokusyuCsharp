a4e8bd6847258940de56a2605c8741dd36d3ec9f
Toshiki-Sasaki <sasaki.toshiki.sm9@is.naist.jp>
2019-05-20 02:20:52 +0900

    add 理解度チェック3.sln

---
+++ chap4/理解度チェック3/理解度チェック3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace 理解度チェック3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var tmp in args)
+            {
+                int i = Int32.Parse(tmp);
+                Console.WriteLine(i*1.5);
+            }
+
+        }
+    }
+}
