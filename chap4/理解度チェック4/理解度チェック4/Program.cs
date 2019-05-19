0ba2fadb7dab516396e48140c5f3fa4863f0c95c
Toshiki-Sasaki <sasaki.toshiki.sm9@is.naist.jp>
2019-05-20 02:37:45 +0900

    add 理解度チェック4&5

---
+++ chap4/理解度チェック4/理解度チェック4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace 理解度チェック4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var language = "Go";
+
+            switch (language)
+            {
+                case "C#":
+                case "Visual Basic":
+                case "F#":
+                    Console.WriteLine(".NET対応言語");
+                    break;
+
+                case "Python":
+                case "Ruby":
+                    Console.WriteLine("スクリプト言語");
+                    break;
+
+                default:
+                    Console.WriteLine("不明");
+                    break;
+            }
+        }
+    }
+}
---
+++ chap4/理解度チェック5/理解度チェック5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace 理解度チェック5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var language = "Visual Basic";
+
+            if (language=="C#" || language=="Visual Basic" || language=="F#")
+            {
+                Console.WriteLine(".NET対応言語");
+            }
+            else if (language=="Python" || language=="Ruby")
+            {
+                Console.WriteLine("スクリプト言語");
+            }
+            else
+            {
+                Console.WriteLine("不明");
+            }
+        }
+    }
+}
